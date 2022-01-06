a66cd1ed3e5d7d6e6d54d7e22a0c2030e62aac7f
limichaelc <li.michael.c@gmail.com>
2022-01-06 14:06:25 -0800

    Update any.js

--- src/Commands/any.js
+++ src/Commands/any.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { allAdventurers } = require('adventurers');
+const { allAdventurers } = require('../adventurers');
 
 module.exports = {
   data: new SlashCommandBuilder()
