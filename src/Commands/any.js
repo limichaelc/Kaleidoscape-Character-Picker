512bd0de26b5992b0eca48ba8a3deefdbcdbcf0a
limichaelc <li.michael.c@gmail.com>
2022-01-06 14:15:50 -0800

    Update any.js

--- src/Commands/any.js
+++ src/Commands/any.js
@@ -4,8 +4,15 @@ const { allAdventurers } = require('../adventurers');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("any")
-    .setDescription("Picks any random character from the full adventurer pool"),
+    .setDescription("Picks a random character")
+    .addStringOption(option =>
+      option.setName('element')
+        .setDescription('Specify the character element')
+        .setRequired(false)
+        .addChoice('flame', 'flame')),
   execute: async (interaction, client) => {
+    const element = interaction.data.options.find(option => option.name === 'element');
+    console.log(element);
     var item = allAdventurers[Math.floor(Math.random()*allAdventurers.length)];
     return interaction.reply(item);
   },
