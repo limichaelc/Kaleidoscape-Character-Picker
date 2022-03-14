447f440a679fa45eb6b3f165da0ad116e749f748
limichaelc <li.michael.c@gmail.com>
2022-03-14 10:41:08 -0700

    Update prints.js

--- src/prints.js
+++ src/prints.js
@@ -196,7 +196,7 @@ const ABILITY_EMOJI = {
 };
 
 function emoji(type) {
-  return `<:${type.replaceAll(' ', '')}:>`;
+  return `:${type.replaceAll(' ', '')}:`;
 }
 
 function isHitterAbility(type) {
